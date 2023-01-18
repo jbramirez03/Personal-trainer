f4b0c6b99d7a7af3211db2c234684273f80207eb
temptado <jason1287712@gmail.com>
2023-01-18 05:45:23 -0500

    added info box into welcome container

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,21 @@ const Home = () => {
         <div className='welcome__container'>
             <h1 className='welcome__title'>Resist Mediocrity, <br />Create More</h1>
             <h3 className='welcome__subtitle'>Professional training at your disposal to create the best version of yourself.</h3>
+
+            <div className="welcome__info">
+                <div className="info__box">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum dolor sit amet,<br />consectetur adipiscing elit.</p>
+                </div>
+                <div className="info__box">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum dolor sit amet,<br />consectetur adipiscing elit.</p>
+                </div>
+                <div className="info__box">
+                    <h3>Lorem</h3>
+                    <p>Lorem ipsum dolor sit amet,<br />consectetur adipiscing elit.</p>
+                </div>
+            </div>
         </div>
     )
 }
