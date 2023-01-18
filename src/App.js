36ede51fad091958c56359d17d3bac690b64c17e
temptado <jason1287712@gmail.com>
2023-01-17 23:04:08 -0500

    imported components into app js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Hero from './components/Hero';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import Services from './components/services';
 import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <Hero></Hero>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Hero />}>
+
+        </Route>
+        <Route path='/testimonial'></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import Home from './Home';
 const hero = () => {
     return (
         <div className='hero'>
-            <img src={heroImg} alt="hero image" className='hero__img' />
+            <img src={heroImg} alt="Deadlift barbell" className='hero__img' />
             <Home></Home>
         </div>
     )
