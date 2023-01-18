ca547cb7522425fd3b3e2d31caf10373a7b69be3
temptado <jason1287712@gmail.com>
2023-01-18 05:08:49 -0500

    added new build and gh pages repository

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 import Services from './components/Services';
+import Testimonials from './components/Testimonials';
 import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -14,7 +15,7 @@ function App() {
       <Routes>
         <Route path='/' element={<><Hero /> <Header /></>}>
         </Route>
-        <Route path='/testimonial'></Route>
+        <Route path='/testimonial' element={<Testimonials></Testimonials>}></Route>
       </Routes>
     </Router>
   );
